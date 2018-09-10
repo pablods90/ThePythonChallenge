5d3e6d55ea2205a43d18fdab01aef28022e64119
Pablo Andres <pablods90@users.noreply.github.com>
2018-09-10 00:39:30 -0300

    Solution to challenge 02!

--- 02/challenge02.py
+++ 02/challenge02.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
 # @Patan
 # Challenge 02: http://www.pythonchallenge.com/pc/def/ocr.html
-# Comments:     None
+# Comments:     - You need to inspect the HTML and get the raw text from there.
+#               - Be careful the string is UNICODE!
+
+# Imports
+import codecs
+import string
+
+# Variables
+alphabet = tuple(string.ascii_letters + string.digits)
+
+# Open the file and get the raw text (it is unicode!)
+raw_text = codecs.open('raw_text.txt','rt',encoding='utf-8').read()
+
+# Apply a filter
+pretty_string = filter(lambda character: character in alphabet,raw_text)
+
+# Print decoded string
+print "\nHidden String:\n"
+print pretty_string + "\n"
