0c9c4b518b879cbc8c118c14f991fc7c780301a9
Pablo Andres <pablods90@users.noreply.github.com>
2018-09-11 03:29:46 -0300

    Some progress in the regexp but still missinge the clue.

--- 03/challenge03.py
+++ 03/challenge03.py
@@ -5,17 +5,23 @@
 #           the hint (exactly three big bodyguads on each side)... we can guess
 #           that we need to find a letter that is "surrounded" by a regular
 #           expression that repeats 3 times on each side of the letter.
+#           - Good help: https://www.machinelearningplus.com/python/python-regex-tutorial-examples/
+#           - \W is equivalent to [^a-zA-Z_0-9]
 
 # Imports
 import re
 
-# Open the file and get the raw text
-raw_text = open('raw_text.txt','rt').read()
+# Open the file and get the raw string (convert it to one line string)
+raw_text = "".join( open('raw_text.txt','rt').read().splitlines() )
 
-# Apply a filter
-reg_exp = r'(.*){3}\w(.*){3}'
-hidden_letter = re.search(reg_exp,raw_text,re.M|re.I)
+# Apply a RegExp to retrieve the parttern
+reg_exp = r'(\B[A-Za-z]+\B)\1{2}[A-Za-z]\1{3}'
+#reg_exp = r'[A-Z]{3}[a-z][A-Z]{3}'
+findings = re.search(reg_exp,raw_text,re.M)
 
-# Print hidden letter
-print "\nHidden String:\n"
-print hidden_letter.group() + "\n"
+# Show the results
+if findings:
+  print '\nFound: ', findings.group()
+  print "\nHidden String:\n"
+else:
+  print 'Nothing found!'
