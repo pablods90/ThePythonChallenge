7b275b72e0c71e077ded39ee7645fa36725fcc6c
Pablo Andres <pablods90@users.noreply.github.com>
2018-09-12 03:06:49 -0300

    Solving challenge 03!

--- 03/challenge03.py
+++ 03/challenge03.py
@@ -6,7 +6,8 @@
 #           that we need to find a letter that is "surrounded" by a regular
 #           expression that repeats 3 times on each side of the letter.
 #           - Good help: https://www.machinelearningplus.com/python/python-regex-tutorial-examples/
-#           - \W is equivalent to [^a-zA-Z_0-9]
+#           - \w is equivalent to [^a-zA-Z_0-9].
+#           - The key is "EXACTLY THREE" (not, more than three).
 
 # Imports
 import re
@@ -15,14 +16,35 @@ import re
 raw_text = "".join( open('raw_text.txt','rt').read().splitlines() )
 
 # Apply a RegExp to retrieve the parttern
-#reg_exp = r'(\B[A-Za-z]+\B)\1{2}[A-Za-z]\1{3}'
-#reg_exp = r'[A-Z]{3}[a-z][A-Z]{3}'
-reg_exp = r'([A-Z])\1{2}[a-z]([A-Z])\2{2}'
-findings = re.search(reg_exp,raw_text,re.M)
+reg_exp = r'[^A-Z][A-Z]{3}[a-z][A-Z]{3}[^A-Z]'
+findings = re.findall(reg_exp,raw_text,re.M)
 
 # Show the results
 if findings:
-  print '\nFound: ', findings.group()
-  print "\nHidden String:\n"
+
+    print '\nFound: ', findings, "\n"
+
+    # Findings is a list of strings that match the rule. The rule is matched with
+    # a string like this (showing the string):
+    #   - [0] 1 lowercase letter
+    #   - [1] 1 Capital leter
+    #   - [2] 1 Capital leter
+    #   - [3] 1 Capital leter
+    #
+    #   - [4] 1 lowercase letter (our key letter, the one we need to retrieve)
+    #
+    #   - [5] 1 Capital leter
+    #   - [6] 1 Capital leter
+    #   - [7] 1 Capital leter
+    #   - [8] 1 lowercase letter
+    #
+    # If we keep the 4th position of each string we can get our keyword...
+    #
+    keyword=""
+    for match in findings:
+        keyword+=match[4]
+
+    print '\nOur keyword is: ', keyword, "\n"
+
 else:
-  print 'Nothing found!'
+    print 'Nothing found!'
