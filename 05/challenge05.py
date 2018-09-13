da6050f75bf32f0a0aad453ea3fd25389edf1f75
Pablo Andres <pablods90@users.noreply.github.com>
2018-09-13 01:50:07 -0300

    Adding some comments.

--- 05/challenge05.py
+++ 05/challenge05.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 # @Patan
 # Challenge 05: http://www.pythonchallenge.com/pc/def/peak.html
-# Comments: None
+# Comments: - "Peak hell" sounds like... Pickle!
+#           - There is a file named "banner.p" in the HTML source code that looks
+#           a bit strange. You just need a simple '.py' to create a pickled object
+#           by yourself, open it with a text editor and check how it looks like.
+#           - You may not know how a "pickled" object looks like... but for sure
+#           you can tell it looks really similar to the "banner.p" object. We can
+#           guess then that we have found our pickled object.
+#           - WARNING: Never unpickle an object from any untrusted source. By
+#           untrusted source I mean anything that was not pickled by YOU. I do not
+#           trust any pickled object, not even the one on this challenge. I suggest
+#           using a VM for working with this script (if possible, not running on your
+#           personal computer).
+
+# Imports
+import pickle
