c79e324c40aa50cb5aaef6cde7665d1f951b5e37
Pablo Andres <pablods90@users.noreply.github.com>
2018-09-15 00:16:04 -0300

    Solved challenge 05!

--- 05/challenge05.py
+++ 05/challenge05.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# @Patan
+# @Patan / @manuk
 # Challenge 05: http://www.pythonchallenge.com/pc/def/peak.html
 # Comments: - "Peak hell" sounds like... Pickle!
 #           - There is a file named "banner.p" in the HTML source code that looks
@@ -11,8 +11,28 @@
 #           - WARNING: Never unpickle an object from any untrusted source. By
 #           untrusted source I mean anything that was not pickled by YOU. I do not
 #           trust any pickled object, not even the one on this challenge. I suggest
-#           using a VM for working with this script (if possible, not running on your
-#           personal computer).
+#           using a VM for working with this script (if possible, not running on
+#           your personal computer).
 
 # Imports
 import pickle
+
+# Helpers
+decode = lambda (x,y) : x*y
+
+# Variables
+result = ""
+
+# Get the piclked object
+my_data = pickle.load( open("./banner.p","rb") )
+
+# We discovered that the pickled object is a list of lists... It has in each
+# position the character to print ("#") and the number of repetitions. We use
+# the function reduce to iterate over the elements of the list, generate the
+# banner and then print it!
+for list in my_data:
+    result += reduce( lambda a,b : a + decode(b), list , "" )
+    result += '\n'
+
+# Print the banner!
+print result
