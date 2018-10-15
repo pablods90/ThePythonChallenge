a1ffa9dd3865c101874b0ab348008381902ec691
Pablo Andres <pablods90@users.noreply.github.com>
2018-10-15 18:20:29 -0300

    Fixes and progress!

--- 06/challenge06.py
+++ 06/challenge06.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # @Patan
 # Challenge 06: http://www.pythonchallenge.com/pc/def/channel.html
-# Comments: None
+# Comments: This is one of the trickest challenges... I spent a lot of time
+#           playing with Python ZIP method, trying to combine lists from the
+#           previous pickled object. I really lost a lot of time... until while
+#           I was running out of ideas I tried replacing the ".html" for a ".zip"
+#           in the URL. Got it! Yo download a .zip file full of nothnigs...
 
 # Imports
 import time
@@ -11,23 +15,19 @@ import zipfile
 
 # Variables
 txt_file = {'nothing': '90052'}
-base_dir = "channel"
+zip_name = "channel.zip"
 all_nothings = []
 comments = ""
 
-# Get the .txt files path
-base_dir = os.path.join(os.getcwd(),base_dir)
+# Get the zip file path
+zip_name = os.path.join(os.getcwd(),zip_name)
 
 # Store the first Nothing
 all_nothings.append(txt_file['nothing'])
 
-# Now we have to play with the comments from the zipped file,
-# as the last nothing suggested...
-# "Collect the comments"
-with zipfile.ZipFile(base_dir + '.zip','r') as the_zip:
-
-    # Get a list of all the files
-    all_the_files = the_zip.namelist()
+# Now we have to go through all the files, get the name for
+# the next one... and go on until we reach the last file.
+with zipfile.ZipFile(zip_name,'r') as the_zip:
 
     while True:
 
@@ -37,7 +37,7 @@ with zipfile.ZipFile(base_dir + '.zip','r') as the_zip:
         # Get the text from the file...
         file_text = the_zip.open(file_to_open).read()
 
-        # Parse the response to get the new nothing
+        # Parse the text to get the new nothing
         m = re.search(r'Next nothing is ([0-9]+)',file_text,re.U)
 
         # If found a new one... update and keep going.
@@ -46,27 +46,27 @@ with zipfile.ZipFile(base_dir + '.zip','r') as the_zip:
         if m is not None:
             txt_file['nothing'] = m.group(1)
 
+            # Store the comment of the current file
+            comments += the_zip.getinfo(file_to_open).comment
+
+            # Show the progress...
+            print "\nSearching in file: \t\t%s" % file_to_open
+            print "Got new nothing: \t%s" % txt_file['nothing']
+
+            # Nothing loop detector (just in case)...
+            if txt_file['nothing'] not in all_nothings:
+                all_nothings.append(txt_file['nothing'])
+            else:
+                print "Repeated nothing! You are in a loop..."
+                exit(-1)
+
         # If we do not find a match... we assume that we reached the end of
         # the chain.
         else:
             print "\nEnd of the chain!"
             print "\nThe last file is: %s\n" % file_to_open
-            print file_text
+            print "The found text is: %s" % file_text
             break
 
-        # Show the progress...
-        print "\nSearching in file: \t\t%s" % file_to_open
-        print "Got new nothing: \t%s" % txt_file['nothing']
-
-        # Nothing loop detector (just in case)...
-        if txt_file['nothing'] not in all_nothings:
-            all_nothings.append(txt_file['nothing'])
-        else:
-            print "Repeated nothing! You are in a loop..."
-            exit(-1)
-
-        # Store the comment of the current file
-        comments += the_zip.getinfo(file_to_open).comment
-
 # Print the final string
-print comments
+print "\nThe ASCII art is:\n%s" % comments
