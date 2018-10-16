3d5b8199916d84e5d398ef7365125722e9cc27df
Pablo Andres <pablods90@users.noreply.github.com>
2018-10-16 01:06:02 -0300

    Adding the img class. Still needs to be refactored.

--- 07/challenge07.py
+++ 07/challenge07.py
@@ -3,35 +3,49 @@
 # Challenge 07: http://www.pythonchallenge.com/pc/def/oxygen.html
 # Comments: - It is an RGBA image (RGB + Alpha), 4 channels (32 bytes/pixel).
 #           - PNG Size: 629 x 95
-#           - Black line starts at 43 ends in 51.
-#
+#           - Black line starts at row 43 and ends at row 51.
+#           - You need to figure out that each different color in the 'black'
+#           line in the middle of the photo, represents ASCII values of a secret
+#           String.
+#           - We can pick any row between 43 and 51 to get our string.
 
+# Imports
 import numpy as np
 from PIL import Image
+
+# Class
+
+class PNGHandler():
+
+    def __init__(self, img_path):
+        self.img = self._load_image(img_path)
+        self.img_matrix = self._get_matrix(self.img)
+
+    def _load_image(self, img_path):
+        return Image.open(img_path)
+
+    def _get_matrix(self, img):
+        return np.array(img)
+
+# Variables
+raw_text = ''
+the_string = ''
 
 img = Image.open('oxygen.png')
 arr = np.array(img)
 
-raw_text = ""
 
-h = 95
-w = 629
 affected_row = 45
 
 # We hold the image in a 3d array.
 # arr[row][column][R G B A]
 #     95 x  629    0 1 2 3
-s = 45
-e = s + 1
 #for affected_row in range(s, e, 1):
 
 for i, pixel in enumerate(arr[affected_row]):
 
     raw_text += chr(pixel[0])
-    #arr[affected_row,i,0] = 0
-    #arr[affected_row,i,1] = 0
-    #arr[affected_row,i,2] = 0
-the_string = ''
+
 for i, c in enumerate(raw_text):
     if c is not raw_text[i-1]:
         the_string += c
