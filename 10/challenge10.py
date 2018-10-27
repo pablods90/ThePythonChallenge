18099d16b5984a0606dfff7ab1228fe9207ab8d3
Pablo Andres <pablods90@users.noreply.github.com>
2018-10-27 18:38:54 -0300

    Adding details.

--- 10/challenge10.py
+++ 10/challenge10.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 # @Patan
-# Challenge 00: http://www.pythonchallenge.com/pc/def/bull.html
+# Challenge 10: http://www.pythonchallenge.com/pc/def/bull.html
 # Comments: None
+
+# Let see...
+# a = [1, 11, 21, 1211, 111221,
+# len(a[30]) = ?
